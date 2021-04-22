96386f526c8d6c9aa0fcbb04ff6ccc72f38d5c79
muta <muttaroni8827@gmail.com>
2021-04-23 00:04:52 +0900

    feat: handle 404 error

--- app/app.py
+++ app/app.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('not_found.html'), 404
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
