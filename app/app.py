6a2fd55236148466b75498a7e93288eb328e2ddb
muta <muttaroni8827@gmail.com>
2021-05-03 23:23:29 +0900

    refactor: introduce dotenv and modify code styling

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
 import os
+import config
 
 from flask import Flask
 from flask import render_template
@@ -7,17 +8,20 @@ from flask import redirect
 from flask import url_for
 from flask import session
 from werkzeug.utils import secure_filename
+from datetime import timedelta
 
 ABSOLUTE_UPLOAD_FOLDER = './app/static/upload/'
 UPLOAD_FOLDER = '/static/upload/'
+
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = ABSOLUTE_UPLOAD_FOLDER
-app.secret_key = 'this_is_sample_secret_key'
+app.secret_key = config.SECRET_KEY
+app.permanent_session_lifetime = timedelta(minutes=5)
 
 @app.route('/')
 def index():
-    img_path = session['path']
-    if img_path:
+    if 'path' in session:
+        img_path = session['path']
         return render_template('index.html', img_path=img_path)
     else:
         return render_template('index.html')
@@ -35,7 +39,7 @@ def not_found(error):
     return render_template('not_found.html'), 404
 
 # reference: https://flask.palletsprojects.com/en/1.1.x/patterns/fileuploads/
-@app.route('/upload', methods=['GET', 'POST'])
+@app.route('/upload', methods=['POST'])
 def upload_image():
     if 'file' not in request.files:
         return redirect(url_for('index'))
@@ -50,7 +54,7 @@ def upload_image():
         file.save(img_path)
         rendering_img_path = os.path.join(UPLOAD_FOLDER, filename)
         session['path'] = rendering_img_path
-        return redirect(url_for('index'))    
+        return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
