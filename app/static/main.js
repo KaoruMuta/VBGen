989c991438668bf9415da4d54d00f9a375b3d10b
muta <muttaroni8827@gmail.com>
2021-05-04 21:08:18 +0900

    feat: color text

--- app/static/main.js
+++ app/static/main.js
@@ -2,7 +2,7 @@ const telopTextArea = document.getElementById('telop-textarea');
 const inputText = document.getElementById('input-text');
 const previewWrapper = document.getElementById('preview-wrapper');
 const previewImage = document.getElementById('preview-image');
-
+const colorPicker = document.getElementById('color');
 
 function updateTelopText(value) {
   inputText.value = value;
@@ -12,12 +12,18 @@ function clearTextArea() {
   telopTextArea.value = "";
 }
 
+function changeColor() {
+  console.log(colorPicker);
+  telopTextArea.style.color = colorPicker.value;
+}
+
 function addTextInImage() {
   const x = window.pageXOffset + previewImage.getBoundingClientRect().left;
   const y = window.pageYOffset + previewImage.getBoundingClientRect().top;
   let displayText = document.createElement('div');
   displayText.innerHTML = inputText.value;
   displayText.style.position = 'absolute';
+  displayText.style.color = colorPicker.value;
   displayText.style.left = x;
   displayText.style.top = y;
   // insert text in front of a preview image
