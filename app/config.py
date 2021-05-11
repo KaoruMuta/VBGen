19c154d92c8ffd33b70695ebbd3ba7e3971a19dc
muta <muttaroni8827@gmail.com>
2021-05-11 10:29:53 +0900

    style: add comment

--- app/config.py
+++ app/config.py
@@ -2,4 +2,4 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')    # also added key to heroku for deployment (`heroku config:set ..`)
